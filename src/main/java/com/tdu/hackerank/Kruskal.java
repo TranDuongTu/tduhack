64985c0ce62265fbfd7fa91fd077548467975406
Tu Tran <tranduongtu92@gmail.com>
2018-01-17 23:04:54 +0700

    Update Kruskal to be more OOP

--- src/main/java/com/tdu/hackerank/Kruskal.java
+++ src/main/java/com/tdu/hackerank/Kruskal.java
@@ -3,56 +3,114 @@ package com.tdu.hackerank;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class Kruskal {
 
   public static List<int[]> findMST(final List<int[]> input) {
-    final List<int[]> mst = new ArrayList<>();
-    final int n = input.get(0)[0];
-    final int[] uf = new int[n];
-    for (int i = 0; i < uf.length; i++) {
-      uf[i] = i;
-    }
+    final Graph graph = readGraph(input);
+    final List<Edge> edges = graph.edges();
+    edges.sort(Comparator.comparingInt(e -> e.weight));
 
-    int connectedEdges = 0;
-    for (final int[] edge : sortEdgeList(input)) {
-      if (connectedEdges < n - 1) {
-        final int u = edge[0] - 1;
-        final int v = edge[1] - 1;
-        final int w = edge[2];
-        if (!find(uf, u, v)) {
-          unite(uf, u, v);
-          mst.add(new int[]{u, v, w});
-          connectedEdges++;
-        }
+    final DisjointSet disjointSet = new DisjointSet(graph.size());
+    final List<int[]> result = new ArrayList<>();
+    for (final Edge edge : edges) {
+      final int from = edge.from;
+      final int to = edge.to;
+      if (!disjointSet.find(from, to)) {
+        result.add(new int[]{from, to, edge.weight});
+        disjointSet.unite(from, to);
+      }
+      
+      if (disjointSet.size() == 1) {
+        break;
       }
     }
-    
-    return mst;
+    return result;
   }
 
-  private static List<int[]> sortEdgeList(final List<int[]> input) {
-    return input.stream().filter(row -> row.length == 3)
-        .sorted(Comparator.comparingInt(e -> e[2]))
-        .collect(Collectors.toList());
-  }
+  private static Graph readGraph(List<int[]> input) {
+    final int n = input.get(0)[0];
+    final int m = input.get(0)[1];
+    final Graph graph = new Graph(n);
 
-  private static boolean find(final int[] uf, final int u, final int v) {
-    return root(uf, u) == root(uf, v);
-  }
-
-  private static int root(final int[] uf, int u) {
-    while (u != uf[u]) {
-      uf[u] = uf[uf[u]];
-      u = uf[u];
+    for (int i = 0; i < m; i++) {
+      final int[] edge = input.get(i + 1);
+      graph.addEdge(new Edge(edge[0] - 1, edge[1] - 1, edge[2]));
     }
-    return u;
+
+    return graph;
   }
 
-  private static void unite(final int[] uf, final int u, final int v) {
-    final int rootU = root(uf, u);
-    final int rootV = root(uf, v);
-    uf[rootU] = rootV;
+  private static class Graph {
+
+    private final List<Edge> edges = new ArrayList<>();
+    private final int n;
+
+    public Graph(final int n) {
+      this.n = n;
+    }
+
+    public List<Edge> edges() {
+      return edges;
+    }
+
+    public void addEdge(final Edge edge) {
+      this.edges.add(edge);
+    }
+
+    public int size() {
+      return n;
+    }
+  }
+
+  private static class Edge {
+
+    public final int from;
+    public final int to;
+    public final int weight;
+
+    public Edge(int from, int to, int weight) {
+      this.from = from;
+      this.to = to;
+      this.weight = weight;
+    }
+  }
+
+  private static class DisjointSet {
+    private final int[] ids;
+    private int size;
+
+    public DisjointSet(final int size) {
+      this.ids = new int[size];
+      for (int i = 0; i < size; i++) {
+        ids[i] = i;
+      }
+      this.size = size;
+    }
+
+    public void unite(int u, int v) {
+      final int rootU = root(u);
+      final int rootV = root(v);
+      if (rootU != rootV) {
+        ids[rootU] = rootV;
+        size--;
+      }
+    }
+
+    private int root(int u) {
+      while (u != ids[u]) {
+        ids[u] = ids[ids[u]];
+        u = ids[u];
+      }
+      return u;
+    }
+
+    public int size() {
+      return size;
+    }
+
+    public boolean find(int from, int to) {
+      return root(from) == root(to);
+    }
   }
 }
