8c4156479ed370c9e0b2cb5a6630c8bee3151498
Tu Tran <tranduongtu92@gmail.com>
2018-03-13 23:26:38 +0700

    syncing problems when appengine start

--- tduhack-dsa/src/main/java/com/tduhack/dsa/endpoints/AppServlet.java
+++ tduhack-dsa/src/main/java/com/tduhack/dsa/endpoints/AppServlet.java
@@ -2,15 +2,20 @@ package com.tduhack.dsa.endpoints;
 
 import com.tduhack.HasFields;
 import com.tduhack.HasName;
+import com.tduhack.JSON;
+import com.tduhack.Strings;
 import com.tduhack.appengine.DataStore;
 import com.tduhack.appengine.Store;
-import com.tduhack.dsa.Problems;
 import com.tduhack.dsa.entity.Problem;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 import java.util.logging.Level;
@@ -32,13 +37,38 @@ public class AppServlet extends HttpServlet {
     final Set<String> savedProblems = store.query("Problem").stream()
             .map(p -> p.get(HasName.name))
             .collect(Collectors.toSet());
-    final List<HasFields> problems = Problems.getAllProblems();
+    final List<HasFields> problems = getAllProblems();
     for (final HasFields problem : problems) {
       final String name = problem.get(Problem.name);
       final int level = problem.get(Problem.level);
       if (!savedProblems.contains(name)) {
         logger.log(Level.INFO, "Saving problem: " + name);
         store.save(store.create("Problem").set(Problem.name, name).set(Problem.level, level));
+      }
+    }
+  }
+
+  public static List<HasFields> getAllProblems() {
+    final InputStream inputStream = AppServlet.class.getResourceAsStream("/problems.txt");
+    final BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
+    try {
+      br.readLine();
+      String buff;
+      final List<HasFields> problems = new ArrayList<>();
+      while (Strings.isNotEmpty(buff = br.readLine())) {
+        final String[] tokens = buff.split(",");
+        final String name = tokens[0].trim();
+        final int level = Integer.valueOf(tokens[1]);
+        problems.add(JSON.create().set(Problem.name, name).set(Problem.level, level));
+      }
+      return problems;
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    } finally {
+      try {
+        br.close();
+      } catch (IOException e) {
+        throw new RuntimeException(e);
       }
     }
   }
--- tduhack-dsa/src/test/java/com/tduhack/dsa/ProblemsTest.java
+++ tduhack-dsa/src/test/java/com/tduhack/dsa/ProblemsTest.java
@@ -3,6 +3,7 @@ package com.tduhack.dsa;
 import com.tduhack.HasFields;
 import com.tduhack.JSON;
 import com.tduhack.dsa.entity.Problem;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.reflections.Reflections;
 
@@ -12,23 +13,10 @@ import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 public class ProblemsTest {
 
   @Test
-  public void testTotalLevelOfGeneratedProblems() {
-    final int LEVEL = 10;
-    final List<HasFields> problems = Problems.selectProblems(LEVEL);
-    assertThat(problems).isNotEmpty();
-
-    final int sumLevel = problems.stream()
-            .map(p -> p.get(Problem.level))
-            .reduce(0, (x, y) -> x + y);
-    assertThat(sumLevel).isEqualTo(LEVEL);
-  }
-
-  @Test
+  @Ignore
   public void syncProblemsData() throws Exception {
     final List<HasFields> problems = getAnnotatedProblems();
 
