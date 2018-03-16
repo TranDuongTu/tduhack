392f7f9a778f97b63a5b5e5483028bfdc2e8d4c5
Tu Tran <tranduongtu92@gmail.com>
2018-03-16 16:16:24 +0700

    dsa product as separate repository

--- tduhack-products/tduhack-dsa/src/main/java/com/tduhack/dsa/entity/Problem.java
+++
@@ -1,8 +0,0 @@
-package com.tduhack.dsa.entity;
-
-import com.tduhack.Field;
-import com.tduhack.HasName;
-
-public interface Problem extends HasName {
-  Field<Integer> level = new Field<>("level", Integer.class);
-}
